1605a00420f54cf92b8b9b11cfb92fff6000873a
LianHY <lianhaiyun@gmail.com>
2017-02-14 10:32:52 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
                     <Timeline.Item color="green">
                       <div className="ex-header">2015-3 / Udesk（北京沃丰时代数据科技有限公司）</div>
                       <div className="proj">
-                        <p className="proj-line proj-name"><span>项目名称：</span>Udesk客服系统/即时通讯系统（IM）/客服端</p>
+                        <p className="proj-line proj-name"><span>项目名称：</span>智能客服系统/即时通讯系统（IM）/客服端</p>
                         <p className="proj-line proj-content">
                           <span>项目内容：</span>IM客服端，主要使用EmberJs框架和xmpp通信，客服人员在IM工作台统一处理来自PC端，手机端，微信，微博，SDK等平台的客户消息。
                           可以处理包括简单文本，富文本，链接，表情，语音，小视频，微信地理位置，表单，文件，图片等类型的消息。支持区域截图，消息超时处理，敏感词处理，转接会话，同事监控，对话路由，对话质检，
@@ -51,7 +51,7 @@ class App extends Component {
                         </p>
                       </div>
                       <div className="proj">
-                        <p className="proj-line proj-name"><span>项目名称：</span>Udesk客服系统/即时通讯系统（IM）/客户端</p>
+                        <p className="proj-line proj-name"><span>项目名称：</span>智能客服系统/即时通讯系统（IM）/客户端</p>
                         <p className="proj-line proj-content">
                           <span>项目内容：</span>使用客服系统的公司可以通过在网站上嵌入代码的方式在自己的网站上生成IM聊天面板，他们的用户就可以通过IM进行咨询。系统支持多种接入方式和自定义配置，包括聊天系统和访客系统，访客系统主要实时的对访客信息进行处理。
                           聊天系统主要用来咨询客服或者机器人，使用xmpp通信，以及WebSocket，跨域等技术，支持手机模式，内嵌模式和独立窗口模式，浏览器兼容到IE6，支持手机端，支持机器人，用户识别，离线留言，满意度评价，自定义广告和推广消息等功能。
@@ -64,21 +64,21 @@ class App extends Component {
                         </p>
                       </div>
                       <div className="proj">
-                        <p className="proj-line proj-name"><span>项目名称：</span>Udesk客户管理系统（CRM）</p>
+                        <p className="proj-line proj-name"><span>项目名称：</span>客户管理系统（CRM）</p>
                         <p className="proj-line proj-content"><span>项目内容：</span>主要是针对销售人员开发的CRM系统，包括销售线索，商机，客户和联系人等模块，以及针对线索和客户的公海等功能。项目中主要使用EmberJs框架。</p>
                         <p className="proj-line proj-own">
                           <span>主要职责：</span>项目中参与开发自定义字段部分。
                         </p>
                       </div>
                       <div className="proj">
-                        <p className="proj-line proj-name"><span>项目名称：</span>Udesk商务智能系统（BI）</p>
+                        <p className="proj-line proj-name"><span>项目名称：</span>商务智能系统（BI）</p>
                         <p className="proj-line proj-content"><span>项目内容：</span>单独的BI系统，使用EmberJs框架，可配套其他系统使用，支持大部分统计图表，支持多维度和多指标，用户可以自己根据需要生成相应地看板。</p>
                         <p className="proj-line proj-own">
                           <span>主要职责：</span>项目搭建，独立完成表格图标组件，参与设计器的开发。
                         </p>
                       </div>
                       <div className="proj">
-                        <p className="proj-line proj-name"><span>项目名称：</span>Udesk客服系统其他模块</p>
+                        <p className="proj-line proj-name"><span>项目名称：</span>智能客服系统其他模块</p>
                         <p className="proj-line proj-content"><span>项目内容：</span>客服系统中的工单，客户，知识库，监控报表等模块，使用EmberJs框架。</p>
                         <p className="proj-line proj-own">
                           <span>主要职责：</span>基于系统各模块之间的业务联系，参与各模块的开发，开发系统全局共用的某些组件。
