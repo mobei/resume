1d27f07551b9b6ecea614bde00c16ffe60d724b1
mobei <lianhayun@gmail.com>
2017-03-06 13:10:19 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -20,37 +20,39 @@ class App extends Component {
             </Col>
           </Row>
           <Row type="flex" justify="center" gutter={24}>
-            <Col span={14}>
+            <Col span={15}>
               <Card title="个人简介" className="to-bottom personal-card">
-                <p><Icon type="pushpin-o" />&nbsp;&nbsp;喜欢Web开发，三年Web前端工作经验，有已上线的大型项目开发经验，有H5开发经验。</p>
-                <p><Icon type="pushpin-o" />&nbsp;&nbsp;熟练掌握JavaScript，HTML，CSS技术，熟练ES2015，熟练EmberJs框架，熟悉React、Redux、Webpack，喜欢接触新技术。</p>
-                <p><Icon type="pushpin-o" />&nbsp;&nbsp;熟练使用Git，熟练Linux，熟悉Nginx服务器，有NodeJs项目开发经验，对后端技术有一定了解。</p>
-                <p><Icon type="pushpin-o" />&nbsp;&nbsp;工作认真负责，有良好的编程习惯，性格开朗，有较强的学习能力、沟通能力和逻辑思维能力。</p>
-                <p><Icon type="pushpin-o" />&nbsp;&nbsp;有企业服务系统开发经验，负责过复杂业务逻辑的即时通讯（IM）系统。有丰富的跨浏览器开发经验</p>
+                <p><Icon type="pushpin-o" />&nbsp;&nbsp;有丰富Web项目开发经验；有企业服务系统（SaaS）开发经验；独立负责过公司项目的构建、开发、维护。</p>
+                <p><Icon type="pushpin-o" />&nbsp;&nbsp;有丰富EmberJs项目开发经验、跨浏览器开发经验；有跨终端开发经验。</p>
+                <p><Icon type="pushpin-o" />&nbsp;&nbsp;熟练掌握JavaScript，HTML5，CSS3，Ajax，WebSocket等前端技术；熟练ES2015、React、Redux。</p>
+                <p><Icon type="pushpin-o" />&nbsp;&nbsp;熟练使用Linux系统、Git；熟练Gulp、Webpack等构建工具；熟悉NodeJs。</p>
+                <p><Icon type="pushpin-o" />&nbsp;&nbsp;工作认真负责，能快速解决问题，善于沟通，学习能力强。</p>
               </Card>
               <Card title="工作/项目经历">
-                <Timeline.Item>
-                  至今
-                </Timeline.Item>
-                <Timeline>
+                <Timeline pending={<span>至今</span>}>
+                    <Timeline.Item color="green">
+                      <div className="ex-header">2014-4 / 天津皖通科技</div>
+                      <div className="proj">
+                        <p className="proj-line proj-name"><span>项目名称：</span>梅江国际艺术馆官网</p>
+                        <p className="proj-line proj-content"><span>项目内容：</span>为天津梅江国际艺术馆开发的内容管理系统（CMS），同时负责前后端工作，网站地址：<a target="_blank" href="http://www.mj-arts.com/">http://www.mj-arts.com/</a></p>
+                      </div>
+                    </Timeline.Item>
                     <Timeline.Item color="red">
                       <div className="ex-header">2015-3 / Udesk（北京沃丰时代数据科技有限公司）</div>
                       <div className="proj">
                         <p className="proj-line proj-name"><span>项目名称：</span>智能客服系统/即时通讯系统（IM）/客服端</p>
                         <p className="proj-line proj-content">
-                          <span>项目内容：</span>IM客服端，主要使用EmberJs框架和xmpp通信，客服人员在IM工作台统一处理来自PC端，手机端，微信，微博，SDK等平台的客户消息。
-                          可以处理包括简单文本，富文本，链接，表情，语音，小视频，微信地理位置，表单，文件，图片等类型的消息。支持区域截图，消息超时处理，敏感词处理，转接会话，同事监控，对话路由，对话质检，
-                          排队管理，访客邀请等功能。
+                          <span>项目内容：</span>基于EmberJs的IM客服端系统。客服人员在IM工作台统一处理来自PC端，手机端，微信，微博，SDK等平台的客户消息。
+                          可以处理包括文本，表情，语音，视频，地理位置，表单，文件等类型的消息。支持监控，排队管理，访客邀请等众多业务功能。主要用到了Websocket，XMPP等技术。
                         </p>
                         <p className="proj-line proj-own">
-                          <span>主要职责：</span>项目中负责IM客服端前端大部分工作以及管理控制后台大部分前端工作
+                          <span>主要职责：</span>项目中独立负责IM客服端前端工作以及管理控制后前端工作
                         </p>
                       </div>
                       <div className="proj">
                         <p className="proj-line proj-name"><span>项目名称：</span>智能客服系统/即时通讯系统（IM）/客户端</p>
                         <p className="proj-line proj-content">
-                          <span>项目内容：</span>使用客服系统的公司可以通过在网站上嵌入代码的方式在自己的网站上生成IM聊天面板，他们的用户就可以通过IM进行咨询。系统支持多种接入方式和自定义配置，包括聊天系统和访客系统，访客系统主要实时的对访客信息进行处理。
-                          聊天系统主要用来咨询客服或者机器人，使用xmpp通信，以及WebSocket，跨域等技术，支持手机模式，内嵌模式和独立窗口模式，浏览器兼容到IE6，支持手机端，支持机器人，用户识别，离线留言，满意度评价，自定义广告和推广消息等功能。
+                          <span>项目内容：</span>包括基于WebSocket，XMPP通信，跨域等技术的网页聊天系统和基于NodeJs的访客系统。该系统可以嵌入第三方网站，进行在线聊天。支持手机模式，内嵌模式和独立窗口模式，兼容各种浏览器，支持机器人等众多功能。
                         </p>
                         <p className="proj-line proj-own">
                           <span>主要职责：</span>项目中独立负责IM客户端中聊天系统的工作，独立完成js内嵌插件。
@@ -61,37 +63,30 @@ class App extends Component {
                       </div>
                       <div className="proj">
                         <p className="proj-line proj-name"><span>项目名称：</span>客户管理系统（CRM）</p>
-                        <p className="proj-line proj-content"><span>项目内容：</span>主要是针对销售人员开发的CRM系统，包括销售线索，商机，客户和联系人等模块，以及针对线索和客户的公海等功能。项目中主要使用EmberJs框架。</p>
+                        <p className="proj-line proj-content"><span>项目内容：</span>基于EmberJs的CRM系统。 主要是针对销售人员开发的CRM系统，包括销售线索，商机，客户和联系人等模块，以及针对线索和客户的公海等功能。</p>
                         <p className="proj-line proj-own">
-                          <span>主要职责：</span>项目中参与开发自定义字段部分。
+                          <span>主要职责：</span>参与各业务模块开发，负责部分公用组件开发。
                         </p>
                       </div>
                       <div className="proj">
                         <p className="proj-line proj-name"><span>项目名称：</span>商务智能系统（BI）</p>
-                        <p className="proj-line proj-content"><span>项目内容：</span>单独的BI系统，使用EmberJs框架，可配套其他系统使用，支持大部分统计图表，支持多维度和多指标，用户可以自己根据需要生成相应地看板。</p>
+                        <p className="proj-line proj-content"><span>项目内容：</span>基于EmberJs的BI系统。独立的BI系统，可配套其他系统使用，支持大部分统计图表，支持多维度和多指标，用户可以自己根据需要生成相应地看板。</p>
                         <p className="proj-line proj-own">
-                          <span>主要职责：</span>项目搭建，独立完成表格图标组件，参与设计器的开发。
+                          <span>主要职责：</span>参与项目搭建，负责部分公用组件开发。
                         </p>
                       </div>
                       <div className="proj">
-                        <p className="proj-line proj-name"><span>项目名称：</span>智能客服系统其他模块</p>
-                        <p className="proj-line proj-content"><span>项目内容：</span>客服系统中的工单，客户，知识库，监控报表等模块，使用EmberJs框架。</p>
+                        <p className="proj-line proj-name"><span>项目名称：</span>知识库管理系统</p>
+                        <p className="proj-line proj-content"><span>项目内容：</span>基于EmberJs的知识库管理系统。主要管理空间下的文章，以及人员组织结构。</p>
                         <p className="proj-line proj-own">
-                          <span>主要职责：</span>基于系统各模块之间的业务联系，参与各模块的开发，开发系统全局共用的某些组件。
+                          <span>主要职责：</span>参与项目搭建。
                         </p>
-                      </div>
-                    </Timeline.Item>
-                    <Timeline.Item color="green">
-                      <div className="ex-header">2014-4 / 天津皖通科技</div>
-                      <div className="proj">
-                        <p className="proj-line proj-name"><span>项目名称：</span>梅江国际艺术馆官网</p>
-                        <p className="proj-line proj-content"><span>项目内容：</span>为天津梅江国际艺术馆开发的内容管理系统（CMS），同时负责前后端工作，网站地址：<a target="_blank" href="http://www.mj-arts.com/">http://www.mj-arts.com/</a></p>
                       </div>
                     </Timeline.Item>
                 </Timeline>
               </Card>
             </Col>
-            <Col span={8}>
+            <Col span={9}>
               <Card title="基本信息" className="to-bottom personal-card">
                 <p><Icon type="user" />&nbsp;&nbsp;连海云/男/本科</p>
                 <p><Icon type="phone" />&nbsp;&nbsp;18310673100</p>
@@ -100,6 +95,7 @@ class App extends Component {
                 <p>投递职位：Web前端工程师</p>
                 <p>工作经验：3年</p>
                 <p>毕业院校：天津工业大学</p>
+                <p>专&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;业：软件工程</p>
                 <p>在线简历：<a target="_blank" href="http://www.mobei.website/resume">http://www.mobei.website/resume</a></p>
               </Card>
               <Card title="专业技能" className="card-skill to-bottom">
